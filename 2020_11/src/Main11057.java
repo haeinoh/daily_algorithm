fd35db1296a49f5607a466d523fde3682e0b6e74
오해인 <hoh3@mail.csuchico.edu>
2020-11-18 23:26:48 +0900

    11057

---
+++ 2020_11/src/Main11057.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main11057 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		int dp[][] = new int[N][N];
+		
+		
+	}
+}
