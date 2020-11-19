d550dd4fd9ee258aa5a54eb65151e0e61270212f
오해인 <hoh3@mail.csuchico.edu>
2020-11-19 22:23:55 +0900

    11057 오르막 수

--- 2020_11/src/Main11057.java
+++ 2020_11/src/Main11057.java
@@ -5,8 +5,26 @@ public class Main11057 {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		
-		int dp[][] = new int[N][N];
-		
-		
+		int answer = 0;
+		int dp[][] = new int[N+1][10];
+		if(N == 1) answer = 10;
+		else if(N == 2) answer = 55;
+		else {
+			for(int i = 0; i < 10; i++) {
+				dp[1][i] = 1;
+				dp[2][i] = i;
+			}
+			
+			for(int i = 3; i <= N; i++) {
+				for(int j = 1; j < 10; j++) {
+					dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 10007;
+				}
+			}
+			
+			for(int j = 1; j <= N; j++) {
+				for(int i = 0; i < 10; i++) answer += (dp[j][i] % 10007);
+			}
+		}
+		System.out.println(answer % 10007);
 	}
 }
