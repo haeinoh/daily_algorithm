ecea97d3bc64285770180605eabf95a379c5d79f
오해인 <hoh3@mail.csuchico.edu>
2020-11-20 00:46:46 +0900

    42885

---
+++ 2020_11/src/Solution42885.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Solution42885 {
+
+	public static void main(String[] args) {
+		int[] people = {70, 60, 30, 20};
+		
+		int limit = 100;
+		int answer = 0;
+		
+	
+		
+		System.out.println(answer);
+	}
+}
