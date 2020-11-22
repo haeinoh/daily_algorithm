1b1eb8f73ac45b30a790763f0848a876d88f691c
오해인 <hoh3@mail.csuchico.edu>
2020-11-23 00:11:05 +0900

    hmm...

--- 2020_11/src/Solution42885.java
+++ 2020_11/src/Solution42885.java
@@ -3,11 +3,30 @@ import java.util.Arrays;
 public class Solution42885 {
 
 	public static void main(String[] args) {
-		int[] people = {70, 60, 30, 20};
+		int[] people = {70, 50, 80, 50};
+		
+		boolean vtd[] = new boolean[people.length];
 		
 		int limit = 100;
 		int answer = 0;
 		
+		for(int i = 0; i < people.length; i++) {
+			if(!vtd[i]) {
+				boolean flag = false;
+				for(int j = people.length-1; j > i; j--) {
+					if(!vtd[j]) {
+						if(people[i] + people[j] <= limit) {
+							vtd[i] = true;
+							vtd[j] = true;
+							flag = true;
+							answer++;
+							break;
+						}
+					}
+				}
+				if(!flag) answer++;
+			}
+		}
 	
 		
 		System.out.println(answer);
