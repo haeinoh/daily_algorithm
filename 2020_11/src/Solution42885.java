da19347491a3c84609daabe790145f25a2cf9848
오해인 <hoh3@mail.csuchico.edu>
2020-11-23 21:49:16 +0900

    programmers 구명보트

--- 2020_11/src/Solution42885.java
+++ 2020_11/src/Solution42885.java
@@ -4,31 +4,16 @@ public class Solution42885 {
 
 	public static void main(String[] args) {
 		int[] people = {70, 50, 80, 50};
-		
-		boolean vtd[] = new boolean[people.length];
-		
-		int limit = 100;
 		int answer = 0;
-		
-		for(int i = 0; i < people.length; i++) {
-			if(!vtd[i]) {
-				boolean flag = false;
-				for(int j = people.length-1; j > i; j--) {
-					if(!vtd[j]) {
-						if(people[i] + people[j] <= limit) {
-							vtd[i] = true;
-							vtd[j] = true;
-							flag = true;
-							answer++;
-							break;
-						}
-					}
-				}
-				if(!flag) answer++;
-			}
+
+		int limit = 100;
+		Arrays.sort(people);
+		int idx = 0;
+		for(int i = people.length-1; i >= idx; i--) {
+			if(people[i] + people[idx] <= limit) idx++;
+			answer++;
 		}
 	
-		
 		System.out.println(answer);
 	}
 }
