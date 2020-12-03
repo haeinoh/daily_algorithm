0d2a13a0592e710902f60d03db1d538ed0013681
오해인 <hoh3@mail.csuchico.edu>
2020-12-03 23:12:54 +0900

    m 12

---
+++ 2020_12/src/Main2042.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main2042 {
+	public static int N, M, K, arr[], tree[];
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		K = sc.nextInt();
+		
+		arr = new int[N];
+		tree = new int[4*N]; // 
+		
+		
+	}
+}
