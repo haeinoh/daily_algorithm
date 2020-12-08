4bf67d531e0af3215f526a4864bdbbe93dcd6465
오해인 <hoh3@mail.csuchico.edu>
2020-12-08 22:18:50 +0900

    programmers 압축

---
+++ 2020_12/src/Main3176.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+class Pair {
+	int y;
+	int num;
+	
+}
+
+public class Main3176 {
+	public static int N, K, depth[], dp[][], maxDp[][], minDp[][];
+	public static List<List<Pair>> list;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bf.readLine().trim());
+		N = Integer.parseInt(st.nextToken());
+		
+		depth = new int[N+1];
+		dp = new int[N+1][21];
+		maxDp = new int[N+1][21];
+		minDp = new int[N+1][21];
+		
+		list = new ArrayList<>();
+		
+		for(int i = 0; i <= N; i++) list.add(new ArrayList<>());
+		
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(bf.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			
+		}
+		
+		st = new StringTokenizer(bf.readLine());
+		K = Integer.parseInt(st.nextToken());
+		for(int i = 0; i < K; i++) {
+			st = new StringTokenizer(bf.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+		}
+		
+	}
+}
--- 2020_12/src/Solution17684.java
+++ 2020_12/src/Solution17684.java
@@ -1,8 +1,53 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Solution17684 {
-
+	public static List<String> list;
+	public static List<Integer> ans;
 	public static void main(String[] args) {
-		String msg = "KAKAO";
+		String msg = "TOBEORNOTTOBEORTOBEORNOT";
+		int[] answer = {};
 		
+		list = new ArrayList<>();
+		ans = new ArrayList<>();
+		
+		list.add("/");
+		for(int i = 1; i < 27; i++) {
+			list.add(String.valueOf((char)(i+64)));
+		}
+		
+		int index = 0;
+		
+		while(true) {
+			//index값을 기준으로 +1, +2 씩 해서 제일 긴 문자열 (list에 존재하는 것 중에)
+			String str = "";
+			String max = ""; // 
+			String next = ""; // 사전추가 (w+c)
+			for(int i = index; i < msg.length(); i++) {
+				str += String.valueOf(msg.charAt(i));
+				if(list.contains(str) && str.length() >= max.length()) {
+					max = str;
+				} else {
+					next = str;
+					break;
+				}
+			}
+
+			if(list.contains(max)) {
+				ans.add(list.indexOf(max)); // 사전에 이미 존재하면, 정답에 add
+				if(next.equals("")) break;
+			} 
+			
+			if(!list.contains(next)) { // 사전에 없으므로 list에 add
+				list.add(next);
+				index += next.length()-1;
+			}
+		}
+		
+		answer = new int[ans.size()]; 
+		for(int i = 0; i < ans.size(); i++) answer[i] = ans.get(i);
+
+		System.out.println(Arrays.toString(answer));
 	}
 }
