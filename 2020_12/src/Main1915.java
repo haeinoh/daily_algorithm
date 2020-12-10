9d3ef72a08573abc1b2fc99f6775550b27e2e12a
오해인 <hoh3@mail.csuchico.edu>
2020-12-10 14:40:34 +0900

    boj 1915 가장 큰 정사각형

--- 2020_12/src/Main1915.java
+++ 2020_12/src/Main1915.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main1915 {
-	public static int N, M, answer, arr[][], dp[][];
+	public static int N, M, answer, arr[][];
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -13,32 +13,28 @@ public class Main1915 {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		arr = new int[N][M];
-		dp = new int[N][M];
 		answer = 0;
-		
+		boolean flag = false;
 		for(int i = 0; i < N; i++) {
 			st = new StringTokenizer(bf.readLine());
 			String str = st.nextToken();
 			for(int j = 0; j < str.length(); j++) {
 				arr[i][j] = Character.getNumericValue(str.charAt(j));
+				if(arr[i][j] == 1) flag = true;	
 			}
 		}
-		
-		for(int i = 1; i < N; i++) {
-			for(int j = 1; j < M; j++) {
-				if(arr[i][j] == 1) {
-					if(arr[i-1][j] == 1 && arr[i-1][j-1] == 1 && arr[i][j-1] == 1) {
-						dp[i][j] = 1;
-						dp[i][j] += Math.max(dp[i-1][j], Math.max(dp[i-1][j-1], dp[i][j-1]));
-						
-						if(dp[i][j] >= answer) answer = dp[i][j];
-					} 
+		if(N == 1 || M == 1) {
+			if(flag) answer = 1;
+		} else {
+			for(int i = 1; i < N; i++) {
+				for(int j = 1; j < M; j++) {
+					if(arr[i][j] == 1) {
+						arr[i][j] = Math.min(arr[i-1][j], Math.min(arr[i-1][j-1], arr[i][j-1])) + 1;
+						if(arr[i][j] >= answer) answer = arr[i][j];
+					}
 				}
 			}
 		}
-		for(int i[]: dp)
-			System.out.println(Arrays.toString(i));
-		
 		System.out.println(answer*answer);
 	}
 }
