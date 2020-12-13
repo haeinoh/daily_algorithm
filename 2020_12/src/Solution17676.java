f67ad46b2f996557ff180fd9a5e2f46e0df5a122
오해인 <hoh3@mail.csuchico.edu>
2020-12-13 20:37:51 +0900

    programmers 추석 트래픽

--- 2020_12/src/Solution17676.java
+++ 2020_12/src/Solution17676.java
@@ -1,16 +1,43 @@
+import java.util.Arrays;
 
 public class Solution17676 {
 
 	public static void main(String[] args) {
-		String[] lines = {"2016-09-15 01:00:04.002 2.0s","2016-09-15 01:00:07.000 2s"};
+		String[] lines = {"2016-09-15 00:00:00.000 2.3s", "2016-09-15 23:59:59.999 0.1s"}; //test용
+		int answer = 0;
+
 		String str[] = new String[3];
+		double store[][] = new double[lines.length][2]; //store start, end
+		double start = 0; //minStart
 		
 		for(int i = 0; i < lines.length; i++) {
-			str = lines[0].split(" ");
-			String time = str[1];
-			String gap = str[2].replace("s", "");
+			str = lines[i].split(" "); // split first
+			double time = 0;
+ 			String t1 = str[1]; // calc time
+			String cal[] = t1.split(":");
+			time += (3600*Double.parseDouble((cal[0])) + 60*Double.parseDouble((cal[1])) + Double.parseDouble(cal[2]));
 			
-			
+			String g1 = str[2].replace("s", ""); //gap 
+			double gap = Double.parseDouble(g1);
+			store[i][0] = Double.parseDouble(String.format("%.3f", (time-gap+0.001)));
+			store[i][1] =  Double.parseDouble(String.format("%.3f", (time)));
 		}
+
+		double next = 0;
+		for(int i = 0; i < lines.length; i++) {
+			int count = 0;
+			start = store[i][1];
+			next =  Double.parseDouble(String.format("%.3f", (start + 1))); //0.999->1
+			for(int j = 0; j < lines.length; j++) {
+				if(store[j][0] >= start && next > store[j][0]) // start보다는 크거나 같고 next보다는 작게
+					count++;
+				 else if(store[j][1] >= start && next > store[j][1]) 
+					count++;
+				 else if(store[j][0] <= start && next <= store[j][1]) // start와 next값 모두 포함 
+					count++;
+			}
+			if(answer < count) answer = count;
+		}
+		System.out.println("answer " + answer);
 	}
 }
