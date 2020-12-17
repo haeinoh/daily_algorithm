c2b031f41ba16f88438411d447e283e538968de9
오해인 <hoh3@mail.csuchico.edu>
2020-12-18 01:26:54 +0900

    programmers 셔틀버스

--- 2020_12/src/Solution17678.java
+++ 2020_12/src/Solution17678.java
@@ -7,9 +7,11 @@ public class Solution17678 {
 		String answer = "";
 		int n = 2;
 		int t = 10;
-		int m = 2;
-		String timetable[] = {"09:10", "09:09", "08:00"}; // test용
+		int m = 3;
+		String timetable[] = {"09:05", "09:09", "09:13"}; // test용
+//		String timetable[] = {"09:10", "09:09", "08:00"}; // test용
 //		String timetable[] = {"08:00", "08:01", "08:02", "08:03"}; // test용
+		
 		int time[] = new int[timetable.length];
 		
 		for(int i = 0; i < timetable.length; i++) {
@@ -21,15 +23,26 @@ public class Solution17678 {
 		System.out.println(Arrays.toString(time));
 		int start = 9*60;
 		int ans = 0;
+		// 콘은 셔틀을 무조건 타야한다 => 맨 마지막에 탑승할 수 있는 사람의 시간 -1 분
+		
 		int idx = 0;
-		// 버스가 꽉 차서 못 타는 경우 
-		// 버스에 탈 수 있는 경우
+		int count = 0;
 		for(int i = 0; i < n; i++) {
-			
-			
+			count = 0;
+			for(int j = idx; j < time.length; j++) {
+				if(time[j] <= start) {
+					count++;
+				}
+				if(count == m) break; // 정원
+			}
+			idx += count; //index 변경
+			if(i != n-1) start += t; // 시간
 		}
 		
-		if(ans / 60 < 12) answer += "0";
+		if(count < m) ans = start; //자리가 있으면 시작시간
+		else ans = time[idx-1] - 1; // 자리가 없으면 맨 마지막에 타는 크루보다 1분 빨리 오기 
+		
+		if(ans / 60 < 10) answer += "0";
 		answer += (ans/60) + ":";
 		if(ans % 60 < 10) answer += "0";
 		answer += (ans % 60);
