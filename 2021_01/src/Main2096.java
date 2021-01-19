52efd39e954a54635cdd32e4e4258448773ccfbc
오해인 <hoh3@mail.csuchico.edu>
2021-01-19 23:53:36 +0900

    boj 2096 주석

--- 2021_01/src/Main2096.java
+++ 2021_01/src/Main2096.java
@@ -12,29 +12,29 @@ public class Main2096 {
 		StringTokenizer st = new StringTokenizer(bf.readLine().trim());
 
 		N = Integer.parseInt(st.nextToken());
-		min = new int[3];
-		max = new int[3];
+		min = new int[3]; // 최소값을 저장할 배열
+		max = new int[3]; // 최대값을 저장할 배열 
 
 		st = new StringTokenizer(bf.readLine());
-		for (int i = 0; i < 3; i++) {
+		for (int i = 0; i < 3; i++) { // 맨 첫째줄로, max와 min 배열에 값을 먼저 세팅해놓는다.
 			min[i] = Integer.parseInt(st.nextToken());
 			max[i] = min[i];
 		}
 
-		for (int i = 1; i < N; i++) {
-			st = new StringTokenizer(bf.readLine());
+		for (int i = 1; i < N; i++) { // 두번째 줄부터 시작한다.
+			st = new StringTokenizer(bf.readLine()); // 값이 3개밖에 안되므로, 그냥 변수로 입력 받는다.
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
 			
 			int tmp1 = max[0];
 			int tmp2 = max[1];
-			int tmp3 = max[2];
+			int tmp3 = max[2]; // 밑에 계산에서 값이 변하므로 미리 값을 tmp에 저장해놓는다.
 
-			max[0] = Math.max(max[0], max[1]) + a;
-			max[1] = Math.max(tmp1, Math.max(max[1], max[2])) + b;
-			max[2] = Math.max(tmp2, tmp3) + c;
-
+			max[0] = Math.max(max[0], max[1]) + a; // 첫번째 -> 첫번째와 두번째
+			max[1] = Math.max(tmp1, Math.max(max[1], max[2])) + b; //두번째 -> 첫번째와 두번째와 세번째
+			max[2] = Math.max(tmp2, tmp3) + c; // 세번째 -> 두번째와 세번째
+			//min도 max와 같은 방법으로 계산한다.
 			tmp1 = min[0];
 			tmp2 = min[1];
 			tmp3 = min[2];
