5e754bb0417c88304b9e66fee7f66f0168164f2f
오해인 <hoh3@mail.csuchico.edu>
2021-01-23 02:08:40 +0900

    programmers 60058

--- 2021_01/src/Solution60058.java
+++ 2021_01/src/Solution60058.java
@@ -1,6 +1,6 @@
 
 public class Solution60058 {
-	
+	public static String answer;
 	public static int extract(String p) {
 		int left = 0;
 		int right = 0;
@@ -31,37 +31,42 @@ public class Solution60058 {
 		return true;
 	}
 	
-	
+	public static String split(String p) {
+		if(("").equals(p)) return p;
+		
+		while(p.length() > 0) {
+			int idx = extract(p);
+			String u = p.substring(0, idx+1);
+			String v = p.substring(idx+1, p.length());
+			boolean res = check(u);
+			if(res) { // 올바른 괄호 문자열
+				answer += u;
+				p = v;
+			}
+			else {
+				answer += "(";
+				//v에 대해 재귀적으로 수행
+				split(v);
+				answer += ")";
+				for(int i = 1; i < u.length() - 1; i++) {
+					if(u.charAt(i) == '(') answer += ")";
+					else answer += "(";
+				}	
+				p = "";
+			}
+		}
+		return answer;
+	}
 	public static String solution(String p) {
-        String answer = "";
+		answer = "";
         
-        while(p.length() > 0) {
-        	int idx = extract(p);
-        	String u = p.substring(0, idx);
-        	String v = p.substring(idx, p.length()-1);
-        	
-        	boolean res = check(u);
-        	if(res) { // 올바른 괄호 문자열
-        		answer += u;
-        		p = v;
-        	}
-        	else {
-        		answer += "(";
-        		//v에 대해 재귀적으로 수행??
-        		answer += v;
-        		answer += ")";
-        		for(int i = u.length()-1; i > 0; i--) answer += String.valueOf(u.charAt(i));
-        		p = "";
-        	}
-        }
-        
-        return answer;
+        return split(p);
     }
 	
 	public static void main(String[] args) {
 //		String p = "(()())()";
-//		String p = "()))((()";
-		String p = ")(";
+//		String p = ")(";
+		String p = "()))((()";
 		System.out.println(solution(p));
 	}
 }
