ceacd20e82cb0fa1bf691faf29ff474d34978b03
오해인 <hoh3@mail.csuchico.edu>
2021-01-23 19:18:12 +0900

    programmers 베스트 앨범

--- 2021_01/src/Solution42579.java
+++ 2021_01/src/Solution42579.java
@@ -4,27 +4,29 @@ import java.util.*;
 public class Solution42579 {
     public static int[] solution(String[] genres, int[] plays) {
     	  HashMap<String, Integer> map = new HashMap<>();
-          for(int i = 0; i < genres.length; i++) {
+    	  int maxLength = 0;
+          for(int i = 0; i < genres.length; i++) { // 각 장르의 재생횟수 합치기
               if(map.containsKey(genres[i])) {
                   map.put(genres[i], map.get(genres[i])+plays[i]);
               } else map.put(genres[i], plays[i]);
+              maxLength = Math.max(maxLength, map.get(genres[i]));
           }
 
-          List<String> keySetList = new ArrayList<>(map.keySet());
+          List<String> keySetList = new ArrayList<>(map.keySet()); // list에 넣고
           
-          Collections.sort(keySetList, new Comparator<String>() {
-              @Override
+          Collections.sort(keySetList, new Comparator<String>() { // 재생횟수 순으로 오름차순 정리 
+              @Override 
               public int compare(String o1, String o2) {
-            	  return map.get(o2) - map.get(o1);
+            	  return map.get(o2) - map.get(o1); 
               }
           });
           
-         ArrayList<Integer> list = new ArrayList<>();
-         for(int i = 0;i < keySetList.size(); i++) {
+         ArrayList<Integer> list = new ArrayList<>(); 
+         for(int i = 0; i < keySetList.size(); i++) { // 장르 순서대로 돌면서 
         	 int idx = 0;
-        	 int arr[][] = new int[genres.length][2];
-        	 for(int j = 0; j < genres.length; j++) {
-        		 if(genres[j].equals(keySetList.get(i))) {
+        	 int arr[][] = new int[maxLength][2];
+        	 for(int j = 0; j < genres.length; j++) { 
+        		 if(genres[j].equals(keySetList.get(i))) { // 같은 장르
         			 arr[idx][0] = plays[j];
         			 arr[idx++][1] = j;
         		 }
@@ -32,12 +34,13 @@ public class Solution42579 {
         	 Arrays.sort(arr, new Comparator<int[]>() {
 				@Override
 				public int compare(int[] o1, int[] o2) {
-					return o2[0] - o1[0];
+					return o2[0] - o1[0]; // 재생횟수 기준으로 오름차순
 				}
 			});
         	
         	if(arr[0][0] != 0) list.add(arr[0][1]);
         	if(genres.length > 1 && arr[1][0] != 0) list.add(arr[1][1]);
+        	// 4번 : 하나인 경우 이 설정 안 해주면 런타임 에러 
          }
          int answer[] = new int[list.size()];
     	 for(int i = 0; i < list.size(); i++) answer[i] = list.get(i);
