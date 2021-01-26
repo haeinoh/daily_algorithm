65c34d6d16bbeaf9451af3690f012e41082be540
오해인 <hoh3@mail.csuchico.edu>
2021-01-27 00:35:27 +0900

    ing...

--- 2021_01/src/Solution60063.java
+++ 2021_01/src/Solution60063.java
@@ -21,6 +21,7 @@ public class Solution60063 {
 	public static Queue<Dot> q;
 	public static int dx[] = {0, 0, 1, -1};
 	public static int dy[] = {1, -1, 0, 0};
+	public static int rot[] = {1, -1};
 	
 	public static void solve() {
 		loop:while(!q.isEmpty()) {
@@ -49,136 +50,19 @@ public class Solution60063 {
 					}
 				}
 				
-				if(y1 == y2) { //세로
-					if(x1 < x2) { // x1이 위에 있을 떄 
-						for(int i = 0; i < 2; i++) {
-							int nx1 = x1 + dx[i];
-							int ny1 = y1 + dy[i];
-							int nx2 = x2 + dx[i];
-							int ny2 = y2 + dy[i];
-							
-							if(nx1 < 0 || ny1 < 0 || nx2 < 0 || ny2 < 0 || nx1 >= N 
-									|| ny1 >= N || nx2 >= N || ny2 >= N || bo[nx1][ny1] == 1 || bo[nx2][ny2] == 1) continue;
-							
-							if(i == 0) {
-								if(!vtd[x1+1][y1+1][x2][y2]) {
-									q.add(new Dot(x1+1, y1+1, x2, y2));
-									vtd[x1+1][y1+1][x2][y2] = true;
-								}
-								if(!vtd[x1][y1][x2-1][y2+1]) {
-									q.add(new Dot(x1, y1, x2-1, y2+1));
-									vtd[x1][y1][x2-1][y2+1] = true;
-								}
-							} else {
-								if(!vtd[x1+1][y1-1][x2][y2]) {
-									q.add(new Dot(x1+1, y1-1, x2, y2));
-									vtd[x1+1][y1-1][x2][y2] = true;
-								}
-								if(!vtd[x1][y1][x2-1][y2-1]) {
-									q.add(new Dot(x1, y1, x2-1, y2-1));
-									vtd[x1][y1][x2-1][y2-1] = true;
-								}
-							}
-						}
-					} else {
-						for(int i = 0; i < 2; i++) {
-							int nx1 = x1 + dx[i];
-							int ny1 = y1 + dy[i];
-							int nx2 = x2 + dx[i];
-							int ny2 = y2 + dy[i];
-							
-							if(nx1 < 0 || ny1 < 0 || nx2 < 0 || ny2 < 0 || nx1 >= N 
-									|| ny1 >= N || nx2 >= N || ny2 >= N || bo[nx1][ny1] == 1 || bo[nx2][ny2] == 1) continue;
-							
-							if(i == 0) {
-								if(!vtd[x1][y1][x2+1][y2+1]) {
-									q.add(new Dot(x1, y1, x2+1, y2+1));
-									vtd[x1][y1][x2+1][y2+1] = true;
-								}
-								if(!vtd[x1-1][y1+1][x2][y2]) {
-									q.add(new Dot(x1-1, y1+1, x2, y2));
-									vtd[x1-1][y1+1][x2][y2] = true;
-								}
-							} else {
-								if(!vtd[x1][y1][x2+1][y2-1]) {
-									q.add(new Dot(x1, y1, x2+1, y2-1));
-									vtd[x1][y1][x2+1][y2-1] = true;
-								}
-								if(!vtd[x1-1][y1-1][x2][y2]) {
-									q.add(new Dot(x1-1, y1-1, x2, y2));
-									vtd[x1-1][y1-1][x2][y2] = true;
-								}
-							}
-						}
-						
+				if(x1 == x2) { //가로
+					for(int i = 0; i < 2; i++) {
+						if(x1+rot[i] >= N || x2+rot[i] >= N || x1+rot[i] < 0 || x2+rot[i] < 0 || bo[x1+rot[i]][y1] == 1 || bo[x2+rot[i]][y2] == 1) continue;
+						else if(!vtd[x1][y1][x2+1][y2+rot[i]]) q.add(new Dot(x1, y1, x2+1, y2+rot[i]));
+						else if(!vtd[x1+1][y1+rot[i]][x2][y2]) q.add(new Dot(x1+1, y1+rot[i], x2, y2));
 					}
 				} else { // 가로
-				if(y1 < y2) { // y1이 더 왼쪽에 있을 때  
-					for(int i = 2; i < 4; i++) {
-						int nx1 = x1 + dx[i];
-						int ny1 = y1 + dy[i];
-						int nx2 = x2 + dx[i];
-						int ny2 = y2 + dy[i];
-						
-						if(nx1 < 0 || ny1 < 0 || nx2 < 0 || ny2 < 0 || nx1 >= N 
-								|| ny1 >= N || nx2 >= N || ny2 >= N || bo[nx1][ny1] == 1 || bo[nx2][ny2] == 1) continue;
-						
-						if(i == 2) {
-							if(!vtd[x1][y1][x2+1][y2-1]) {
-								q.add(new Dot(x1, y1, x2+1, y2-1));
-								vtd[x1][y1][x2+1][y2-1] = true;
-							}
-							if(!vtd[x1+1][y1+1][x2][y2]) {
-								q.add(new Dot(x1+1, y1+1, x2, y2));
-								vtd[x1+1][y1+1][x2][y2] = true;
-							}
-						} else {
-							if(!vtd[x1][y1][x2-1][y2-1]) {
-								q.add(new Dot(x1, y1, x2-1, y2-1));
-								vtd[x1][y1][x2-1][y2-1] = true;
-							}
-
-							if(!vtd[x1-1][y1+1][x2][y2]) {
-								q.add(new Dot(x1-1, y1+1, x2, y2));
-								vtd[x1-1][y1+1][x2][y2] = true;
-							}
-						}
-					}
-				} else {
 					for(int i = 0; i < 2; i++) {
-						int nx1 = x1 + dx[i];
-						int ny1 = y1 + dy[i];
-						int nx2 = x2 + dx[i];
-						int ny2 = y2 + dy[i];
-						
-						if(nx1 < 0 || ny1 < 0 || nx2 < 0 || ny2 < 0 || nx1 >= N 
-								|| ny1 >= N || nx2 >= N || ny2 >= N || bo[nx1][ny1] == 1 || bo[nx2][ny2] == 1) continue;
-						
-						if(i == 2) {
-							if(!vtd[x1+1][y1-1][x2][y2]) {
-								q.add(new Dot(x1+1, y1-1, x2, y2));
-								vtd[x1+1][y1-1][x2][y2] = true;
-							}
-							if(!vtd[x1][y1][x2+1][y2+1]) {
-								q.add(new Dot(x1, y1, x2+1, y2+1));
-								vtd[x1][y1][x2+1][y2+1] = true;
-							}
-						} else {
-//							if(!vtd[x1-1][y1-1][x2][y2]) {
-//								q.add(new Dot(x1-1, y1-1, x2, y2));
-//								vtd[x1-1][y1-1][x2][y2] = true;
-//							}
-//							
-//							if(!vtd[x1][y1][x2-1][y2+1]) {
-//								q.add(new Dot(x1, y1, x2-1, y2+1));
-//								vtd[x1][y1][x2-1][y2+1] = true;
-//							}
-						}
+						if(y1+rot[i] >= N || y2+rot[i] >= N || y1+rot[i] < 0 || y2+rot[i] < 0 || bo[x1][y1+rot[i]] == 1 || bo[x2][y2+rot[i]] == 1) continue;
+						else if(!vtd[x1+rot[i]][y1+1][x2][y2]) q.add(new Dot(x1+rot[i], y1+1, x2, y2));
+						else if(!vtd[x1][y1][x2+rot[i]][y2+1]) q.add(new Dot(x1, y1, x2+rot[i], y2+1));
 					}
-					
 				}
-			}
-				
 				size--;
 			}
 			answer++;
