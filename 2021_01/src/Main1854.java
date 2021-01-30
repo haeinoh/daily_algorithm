a8c121c532354a909c988a26bab241e57bcebf77
오해인 <hoh3@mail.csuchico.edu>
2021-01-31 01:54:15 +0900

    boj 1854 k번째 최단경로

--- 2021_01/src/Main1854.java
+++ 2021_01/src/Main1854.java
@@ -1,17 +1,27 @@
 //https://www.acmicpc.net/problem/1854
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main1854 {
-	public static int N, M, K, arr[][];
+	public static int N, M, K;
+	public static List<List<Doro>> list;
 	public static PriorityQueue<Doro> pq;
 	public static PriorityQueue<Integer> dist[];
+	
+	static Comparator<Integer> com = new Comparator<Integer>() { // 내림차순으로 정렬 
+		@Override
+		public int compare(Integer o1, Integer o2) {
+			return o2 - o1;
+		}
+	};
 	
 	public static class Doro implements Comparable<Doro>{
 		int node;
@@ -23,15 +33,13 @@ public class Main1854 {
 	
 		@Override
 		public int compareTo(Doro doro) {
-			return doro.time = this.time;
+			return this.time - doro.time;
 		}
 	}
 	
 	public static void solve() {
-//		for(int i = 1; i <= N; i++) {
-//			dist[i].add(arr[1][i]);
-//		}
-		dist[1].add(0);
+		PriorityQueue<Doro> pq = new PriorityQueue<>();
+		dist[1].add(0); 
 		pq.add(new Doro(1, 0));
 		
 		while(!pq.isEmpty()) {
@@ -39,19 +47,14 @@ public class Main1854 {
 			int cur = tmp.node;
 			int time = tmp.time;
 			
-			for(int i = 1; i <= N; i++) {
-				if(arr[cur][i] > 0) {
-					System.out.println(i + " " + dist[i].size() + " ::  " + (time+arr[cur][i]) + "  " + dist[i].peek());
-					
-					if(dist[i].size() < K) { //K개이하
-						dist[i].add(time+arr[cur][i]);
-						pq.add(new Doro(i, time+arr[cur][i]));
-					} else if(time + arr[cur][i] < dist[i].peek()){
-						System.out.println(" ????????? " + dist[i].peek());
-						dist[i].poll();
-						dist[i].add(time+arr[cur][i]);
-						pq.add(new Doro(i, time + arr[cur][i]));
-					}
+			for(Doro d: list.get(cur)) {
+				if(dist[d.node].size() < K) {
+					dist[d.node].add((time + d.time));
+					pq.add(new Doro(d.node, (time + d.time)));
+				} else if(dist[d.node].peek() > time + d.time){ // K만큼만 체크하게 되면 가장 큰 값이 K번쨰 최단경로가 된다.
+					dist[d.node].poll();
+					dist[d.node].add(time + d.time);
+					pq.add(new Doro(d.node, (time + d.time)));
 				}
 			}
 		}
@@ -63,18 +66,23 @@ public class Main1854 {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
-		arr = new int[N+1][N+1];
+		
+		list = new ArrayList<>();
+		dist= new PriorityQueue[N+1]; 
+		for(int i = 0; i <= N; i++) {
+			list.add(new ArrayList<>());
+			dist[i] = new PriorityQueue<>(com);
+		}
 		
 		for(int i = 0; i < M; i++) {
 			st = new StringTokenizer(bf.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
-			arr[a][b] = c;
+
+			list.get(a).add(new Doro(b, c));
 		}
-		dist = new PriorityQueue[N+1]; // ??? 어떻게 정렬해여??
-		for(int i = 1; i <= N; i++) dist[i] = new PriorityQueue<>();
-		pq = new PriorityQueue<>();
+		 
 
 		solve();
 		for(int i = 1; i <= N; i++) {
