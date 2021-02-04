903ccb176b4317a8fbf379558557c656c54a9a0c
오해인 <hoh3@mail.csuchico.edu>
2021-02-05 00:00:40 +0900

    .

--- 2021_02/src/Solution72414.java
+++ 2021_02/src/Solution72414.java
@@ -6,58 +6,55 @@ public class Solution72414 {
 	}
 
 	public static String solution(String play_time, String adv_time, String[] logs) {
-		String answer = "";
-		if (play_time.equals(adv_time))
-			return "00:00:00";
-		int play = change(play_time);
-		int ad = change(adv_time);
-		int arr[] = new int[play + 1];
+		 String answer = "";
+	        if(play_time.equals(adv_time)) return "00:00:00";
+	        int play = change(play_time);
+	        int ad = change(adv_time);
+	        int arr[] = new int[play+1];
 
-		for (int i = 0; i < logs.length; i++) {
-			String sp[] = logs[i].split("-");
-			int start = change(sp[0]);
-			int end = change(sp[1]);
-			for (int j = start; j <= end; j++)
-				arr[j]++;
-		}
-		int start = 0;
-		int end = 0;
-		int ans = 0;
-		int sum = 0;
-		int cnt = 0;
-		int val = 0;
-//      System.out.println("answer : " + change("01:30:59"));
-		while (start < play) {
-			if (ad <= cnt) {
-				sum -= arr[start++];
-				cnt--;
-			} else if (end == play)
-				break;
-			else {
-				sum += arr[end++];
-				cnt++;
-			}
-			if (ad == cnt) {
-				if (val < sum) {
-					val = sum;
-					ans = start;
-				}
-			}
-		}
-		// ans-=1;
-		int a = ans / 3600;
-		int b = (ans % 3600) / 60;
-		int c = (ans % 3600) % 60;
-		if (a < 10)
-			answer += "0";
-		answer += (a + ":");
-		if (b < 10)
-			answer += "0";
-		answer += (b + ":");
-		if (c < 10)
-			answer += "0";
-		answer += (c);
-		return answer;
+	        for(int i = 0; i < logs.length; i++) {
+	            String sp[] = logs[i].split("-");
+	            int start = change(sp[0]);
+	            int end = change(sp[1]);
+	            arr[start]++;
+	            arr[end]--;
+	        }
+	        int start = 1;
+	        int end = 1;
+	        long ans = 0;
+	        long startSum = 0;
+	        long endSum = 0;
+	        long cnt = 0;
+	        long val = 0;
+	        while(true) {
+	            if(ad <= cnt) {
+	                startSum += arr[start];
+	                start++;
+	                cnt--;
+	            } else if(end == play) break;
+	            else {
+	                endSum += arr[end-1];
+	                end++;
+	                cnt++;
+	            }
+	            if(ad == cnt) {
+	                if(val < (startSum + endSum)) {
+	                    val = (startSum + endSum);
+	                    ans = start;
+	                }
+	            }
+	        }
+	        ans -= 1;
+	        long a = ans / 3600;
+	        long b = (ans % 3600) / 60;
+	        long c = (ans % 3600) % 60;
+	        if(a < 10) answer += "0";
+	        answer += (a + ":");
+	        if(b < 10) answer += "0";
+	        answer += (b + ":");
+	        if(c < 10) answer += "0";
+	        answer += (c);
+	        return answer;
 	}
 
 	public static void main(String[] args) {
---
+++ 2021_02/src/Solution72415.java
@@ -0,0 +1,118 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution72415 {
+	public static class Card {
+		int r;
+		int c; 
+		int ox;
+		int oy;
+		boolean flag;
+		int arr[][];
+		public Card(int r, int c, int[][] arr, boolean flag) {
+			this.r = r;
+			this.c = c;
+			this.arr = arr;
+			this.flag = flag;
+		}
+		
+		public Card(int r, int c, int ox, int oy, int[][] arr, boolean flag) {
+			this.r = r;
+			this.c = c;
+			this.ox = ox;
+			this.oy = oy;
+			this.arr = arr;
+			this.flag = flag;
+		}
+	}
+	public static int N ;
+	public static Queue<Card> q;
+	public static int dx[] = {-1, 0, 1, 0};
+	public static int dy[] = {0, -1, 0, 1};
+	public static boolean vtd[][];
+	
+	public static boolean done(int [][]b) {
+		for(int i = 0; i < N; i++) 
+			for(int j = 0; j < N; j++)
+				if(b[i][j] == 0) return false;
+		
+		return true;
+	}
+	
+	public static int calIndex(int point, int idx, int[][] b) {
+		
+		
+		return 0;
+	}
+	
+	
+	public static int solution(int[][] board, int r, int c) {
+        int answer = 0;
+        N = 4;
+        q = new LinkedList<>();
+        q.add(new Card(r, c, -1, -1, board, false));
+        vtd = new boolean[N][N];
+        loop:while(!q.isEmpty()) {
+        	int size = q.size();
+        	while(size > 0) {
+        		Card tmp = q.poll();
+        		int x = tmp.r;
+        		int y = tmp.c;
+        		int ox = tmp.ox;
+        		int oy = tmp.oy;
+        		int arr[][] = tmp.arr;
+        		boolean flag = tmp.flag;
+        		if(done(arr)) break loop;
+        	
+        		// 4 가지 방향으로 단순 이동
+        		for(int i = 0; i < 4; i++) {
+        			int nx = x + dx[i];
+        			int ny = y + dy[i];
+        			
+        			if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
+        			if(vtd[nx][ny] != flag) {
+        				q.add(new Card(nx, ny, ox, oy, arr, flag));
+        				vtd[nx][ny] = flag;
+        			}
+        		}
+        		
+        		for(int i = 0; i < 4; i++) {
+        			
+        		
+        		}
+        		
+        		if(ox == -1 && oy == -1) {
+        			ox = x;
+        			oy = y;
+        			
+        		} else {
+        			if(arr[ox][oy] == arr[x][y]) {
+        				arr[ox][oy] = 0;
+        				arr[x][y] = 0;
+        				q.add(new Card(x, y, -1, -1, arr, flag));
+        			} else {
+        				q.add(new Card(x, y, -1, -1, arr, flag));
+        			}
+        		}
+        		
+        	}
+        	answer++;
+        }
+        
+        return answer;
+    }
+
+	public static void main(String[] args) {
+		int bo[][] = {
+			{1,0,0,3},
+			{2,0,0,0},
+			{0,0,0,2},
+			{3,0,1,0}	
+		};
+		
+		int r = 1;
+		int c = 0;
+		
+		System.out.println(solution(bo, r, c));
+	}
+}
