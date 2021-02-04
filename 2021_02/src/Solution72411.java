4dd0aab52506970e7a1837462d7802c08706b034
오해인 <hoh3@mail.csuchico.edu>
2021-02-04 20:40:58 +0900

    programmers 메뉴리뉴얼

--- 2021_02/src/Solution72411.java
+++ 2021_02/src/Solution72411.java
@@ -4,43 +4,52 @@ public class Solution72411 {
     public static ArrayList<String> ans;
     public static int arr[];
     public static boolean vtd[];
-    public static void solve(String orders, int course, int count, int num, int idx) {
-        if(count == course) {
-            String str = "";
-            for(int i = 0; i < count; i++) {
-                str += String.valueOf(orders.charAt(arr[i]));
-            }
-            if(map.containsKey(str)) {
-                int val = map.get(str);
-                map.put(str, val+1);
-            } else map.put(str, 1);
-            return;
-        }
+    public static void solve(String orders, int course, int count, int idx) {
+    	 if(count == course) {
+             String temp[] = new String[course];
+             for(int i = 0; i < count; i++) {
+                 temp[i] = String.valueOf(orders.charAt(arr[i]));
+             }
+             Arrays.sort(temp);
+             String str = "";
+             for(int i = 0; i < temp.length; i++) str += temp[i];
+             if(map.containsKey(str)) {
+                 int val = map.get(str);
+                 map.put(str, val+1);
+             } else map.put(str, 1);
+             return;
+         }
 
-        for(int i = idx; i < orders.length(); i++) {
-            if(!vtd[i]) {
-                vtd[i] = true;
-                arr[count] = i;
-                solve(orders, course, count + 1, num, i);
-                vtd[i] = false;
-            }
-        }
+         for(int i = idx; i < orders.length(); i++) {
+             if(!vtd[i]) {
+                 vtd[i] = true;
+                 arr[count] = i;
+                 solve(orders, course, count + 1, i);
+                 vtd[i] = false;
+             }
+         }
     }
     public static String[] solution(String[] orders, int[] course) {
-         ans = new ArrayList<>();
+    	 ans = new ArrayList<>();
+         int maxLength = 0;
+         for(int i = 0; i < orders.length; i++) maxLength = Math.max(maxLength, orders[i].length()); 
         for(int i = 0; i < course.length; i++) {
-            arr = new int[course[i]];
-            map = new HashMap<>();
-            for(int j = 0; j < orders.length; j++) {
-                vtd = new boolean[orders[j].length()];
-                solve(orders[j], course[i], 0, (j+1), 0);
-            }
-            int findMax = Collections.max(map.values());
-            for(Map.Entry<String, Integer> m: map.entrySet()) {
-                if(m.getValue() == findMax) {
-                    ans.add(m.getKey());
-                }
-            }
+        	if(course[i] <= maxLength) {
+        		arr = new int[course[i]];
+        		map = new HashMap<>();
+        		for(int j = 0; j < orders.length; j++) {
+        			vtd = new boolean[orders[j].length()];
+        			solve(orders[j], course[i], 0, 0);
+        		}
+        		int findMax = Collections.max(map.values());
+        		if(findMax >= 2) {
+        			for(Map.Entry<String, Integer> m: map.entrySet()) {
+        				if(m.getValue() == findMax) {
+        					ans.add(m.getKey());
+        				}
+        			}
+        		}
+        	}
         }
         Collections.sort(ans);
         String answer[] = new String[ans.size()];
@@ -49,8 +58,8 @@ public class Solution72411 {
     }
 	
 	public static void main(String[] args) {
-		String o[] = {"ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"};
-		int c[] = {2, 3, 5};
+		String o[] = {"XYZ", "XWY", "WXA"};
+		int c[] = {2, 3, 4};
 		
 		System.out.println(Arrays.toString(solution(o, c)));
 	}
