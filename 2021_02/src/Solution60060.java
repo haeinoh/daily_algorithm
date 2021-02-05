6372da483749c0d58efa9cd7f15d2345fead0050
오해인 <hoh3@mail.csuchico.edu>
2021-02-05 21:30:46 +0900

    programmers 가사검색

--- 2021_02/src/Solution60060.java
+++ 2021_02/src/Solution60060.java
@@ -13,23 +13,65 @@ class newTrie { // 빈 문자열을 가지는 루트 노드
 }
 */
 public class Solution60060 {
+	static class TrieNode {
+        TrieNode next[];
+        boolean isLast;
+        int count;
+        TrieNode() {
+            next = new TrieNode[26];
+            this.isLast = false;
+            this.count = 0;
+        }
+    }
+
+    static class Trie {
+        TrieNode root;
+        Trie() {
+            root = new TrieNode();
+        }
+
+        void insert(String word) {
+            TrieNode node = root;
+            for(int i = 0; i < word.length(); i++) {
+                int idx = word.charAt(i) - 'a';
+                if(node.next[idx] == null) node.next[idx] = new TrieNode();
+                node = node.next[idx];
+            }
+            node.isLast = true;
+        }
+
+        boolean contains(String word, boolean flag) {
+            TrieNode node = root;
+            for(int i = 0; i < word.length(); i++) {
+                int idx = word.charAt(i) - 'a';
+                if(word.charAt(i) == '?') {
+//                    System.out.println(node.count);
+                    continue;
+                }
+                if(node.next[idx] == null) return false;
+                node = node.next[idx];
+                node.count--;
+            }
+            if(!flag && node.isLast) return true;
+            return false;
+        }
+
+    }
 	public static int[] solution(String[] words, String[] queries) {
-		int answer[] = {};
-		// insert ?
-		
-		
-		
-		// contains ?
-		
-		for(int i = 0; i < queries.length; i++) {
-			if(queries[i].charAt(0) == '?') { // 맨 앞에 오는 경우 
-				
-			} else { // 뒤에 오는 경우
-				
-			}
-		}
-		
-		return answer;
+		 int answer[] = new int[queries.length];
+	        Trie t = new Trie();
+	        for(int i = 0; i < words.length; i++) {
+	            t.insert(words[i]);
+	        }
+	        // insert를 두 번 해야하나....??
+	        for(int i = 0; i < queries.length; i++) {
+	            if(queries[i].charAt(0) == '?') { // 앞
+	                if(t.contains(queries[i], false)) answer[i]++;
+	            } else {
+	                if(t.contains(queries[i], true)) answer[i]++;
+	            }
+	        }
+	        return answer;
 	}
 	public static void main(String[] args) {
 		String w[] = {"frodo", "front", "frost", "frozen", "frame", "kakao"};
