31de38294ecba75c44aacf6758085982bf78d90f
hiMAC <hoh3@mail.csuchico.edu>
2021-02-11 12:45:39 +0900

    programmers 방금그곡

--- 2021_02/src/Solution17683.java
+++ 2021_02/src/Solution17683.java
@@ -1,39 +1,63 @@
+//https://programmers.co.kr/learn/courses/30/lessons/17683
 import java.util.ArrayList;
 
 public class Solution17683 {
     public static String solution(String m, String[] musicinfos) {
         String answer = "";
-
-        for(int i = 0; i < musicinfos.length; i+=4) {
-
-            int time = Integer.parseInt(musicinfos[i+1].split(":")[0] + musicinfos[i+1].split(":")[1])
-                    - Integer.parseInt(musicinfos[i].split(":")[0]+musicinfos[i].split(":")[1]);
-            System.out.println("time " + time);
-
-            // musicinfos[i+3] -> split C, C#, ...
-            ArrayList<String> list = new ArrayList<>();
-            for(int k = 0; k < musicinfos[i+3].length(); k++) {
-                if(musicinfos[i+3].charAt(k) == '#') {
-                    System.out.println(list.get(k-1));
-                    String str = list.get(k-1);
-                    str += String.valueOf(musicinfos[i+3].charAt(k));
-                    list.add(k-1, str);
-                } else list.add(String.valueOf(musicinfos[i+3].charAt(k)));
-            }
-            System.out.println(list.toString());
-            if(time >= musicinfos[i+3].length()) {
-
+        int prevTime = 0;
+        ArrayList<String> music = new ArrayList<>();
+        int index = 0;
+        for(int k = 0; k < m.length(); k++) {
+            char ch = m.charAt(k);
+            if(ch == '#') {
+                music.set(index-1, music.get(index-1)+ch);
             } else {
-
+                music.add(String.valueOf(ch));
+                index++;
             }
         }
+        for(int i = 0; i < musicinfos.length; i++) {
+            String info[] = musicinfos[i].split(",");
+            int time = (Integer.parseInt(info[1].split(":")[0])*60 + Integer.parseInt(info[1].split(":")[1]))
+                    - (Integer.parseInt(info[0].split(":")[0])*60 + Integer.parseInt(info[0].split(":")[1]));
 
+            ArrayList<String> list = new ArrayList<>();
+
+            int idx = 0;
+            for(int k = 0; k < info[3].length(); k++) {
+                char ch = info[3].charAt(k);
+                if(ch == '#') {
+                    list.set(idx-1, list.get(idx-1)+ch);
+                } else {
+                    list.add(String.valueOf(ch));
+                    idx++;
+                }
+            }
+            for(int k = 0; k <= time - music.size(); k++) {
+                int tmp = k;
+                boolean flag = false;
+                for(int j = 0; j < music.size(); j++) {
+                    if(list.get(tmp++%list.size()).equals(music.get(j))) continue;
+                    else{
+                        flag = true;
+                        break;
+                    }
+                }
+                if(!flag) {
+                    if(prevTime < time) {
+                        prevTime = time;
+                        answer = info[2];
+                    }
+                }
+            }
+        }
+        if(("").equals(answer)) return "(None)";
         return answer;
     }
 
     public static void main(String[] args) {
-        String m = "CC#BCC#BCC#BCC#B";
-        String mu[] = {"03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"};
+        String m = "ABC";
+        String mu[] = {"12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"};
         System.out.println(solution(m, mu));
     }
 }
