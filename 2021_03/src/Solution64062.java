e6ccef9fb45b5e05693b67cdcf57cdf7f124410c
hiMAC <hoh3@mail.csuchico.edu>
2021-03-03 00:17:20 +0900

    reset

--- 2021_03/src/Solution64062.java
+++ 2021_03/src/Solution64062.java
@@ -2,29 +2,10 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Solution64062 {
-    static Comparator<Integer> comp = (o1, o2) -> o2 - o1;
 
     public static int solution(int[] stones, int k) {
         int answer = 0;
-        PriorityQueue<Integer> q = new PriorityQueue<>(comp);
-        int start = 0;
-        int end = 0;
-        int count = 0;
 
-        while(start <= end) {
-            if(count >= k) {
-                count--;
-                q.remove(stones[start++]);
-            } else if(end == stones.length) break;
-            else {
-                count++;
-                q.add(stones[end++]);
-            }
-
-            if(count == k) {
-                if(q.peek() <= k) answer = Math.max(answer, q.peek());
-            }
-        }
 
         return answer;
     }
