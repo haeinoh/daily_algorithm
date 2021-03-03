20bfffac80a2fb0efc74aa6b4d4e2c70d092b79b
hiMAC <hoh3@mail.csuchico.edu>
2021-03-04 00:17:26 +0900

    programmers 징검다리 건너기(파라매트릭 서치)

--- 2021_03/src/Solution64062.java
+++ 2021_03/src/Solution64062.java
@@ -1,11 +1,31 @@
-import java.util.Comparator;
-import java.util.PriorityQueue;
+//https://programmers.co.kr/learn/courses/30/lessons/64062
+import java.util.Arrays;
 
 public class Solution64062 {
+    public static boolean check(int mid, int[] stones, int k) {
+        int count = 0;
+        for (int stone : stones) {
+            if (stone - mid < 0) count++;
+            else count = 0;
 
+            if (count == k) return false;
+        }
+        return true;
+    }
     public static int solution(int[] stones, int k) {
         int answer = 0;
+        int start = Arrays.stream(stones).min().getAsInt();
+        int end = Arrays.stream(stones).max().getAsInt();
 
+        if(start == end) return end;
+
+        while(start <= end) {
+            int mid = (start + end) >> 1;
+            if(check(mid, stones, k)) {
+                start = mid + 1;
+                answer = mid;
+            } else end = mid - 1;
+        }
 
         return answer;
     }
