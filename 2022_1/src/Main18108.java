1d80003eb22d5f282ccbdc78fb7c2cf0c76dfbac
hiMAC <hoh3@mail.csuchico.edu>
2022-02-01 23:26:39 +0900

    220201

---
+++ 2022_1/src/Main18108.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main18108 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine().trim());
+        System.out.println(Integer.parseInt(st.nextToken()) - 543);
+    }
+}
