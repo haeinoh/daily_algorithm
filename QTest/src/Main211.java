baa94e6c45599cf232bf7eb41db4b5dd1dc013e8
hiMAC <hoh3@mail.csuchico.edu>
2022-04-05 23:04:47 +0900

    20220405

--- QTest/src/Main211.java
+++ QTest/src/Main211.java
@@ -1,41 +1,39 @@
 import java.io.BufferedReader;
-        import java.io.IOException;
-        import java.io.InputStreamReader;
-        import java.util.StringTokenizer;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main211 {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
         int N = Integer.parseInt(st.nextToken());
-        int[][] arr = new int[N][N];
+        int[][] arr = new int[N][5];
         int answer = 0;
+        int max = Integer.MIN_VALUE;
         for(int i = 0; i < N; i++) {
             st = new StringTokenizer(bf.readLine());
-            for(int j = 0; j < N; j++) {
+            for(int j = 0; j < 5; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        int[] res = new int[N];
+
         for(int i = 0; i < N; i++) {
-            int[] tmp = new int[10];
-            for(int j = 0; j < N; j++) {
-                tmp[arr[j][i]]++;
-            }
-            for(int j = 0; j < N; j++) {
-                if(tmp[arr[j][i]] >= 2) {
-                    res[j]+=tmp[arr[j][i]]-1;
+            int cnt = 0;
+            for (int j = 0; j < N; j++) {
+                for (int k = 0; k < 5; k++) {
+                    if (arr[i][k] == arr[j][k]) {
+                        cnt++;
+                        break;
+                    }
                 }
             }
-        } 
-
-        int max = -987654321;
-        for(int i = 0; i <  N; i++) {
-            if(max < res[i]) {
-                max = res[i];
+            if(cnt > max) {
+                max = cnt;
                 answer = i;
             }
         }
+
         System.out.println(answer+1);
     }
 }
