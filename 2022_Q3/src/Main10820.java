75136e0b79fe715ae3eda2c84aa1dd0d30750ee6
hiMAC <hoh3@mail.csuchico.edu>
2022-07-05 22:25:53 +0900

    20220705

---
+++ 2022_Q3/src/Main10820.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main10820 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String str = "";
+        while ((str = bf.readLine()) != null) {
+            int[] arr = new int[4];
+            for (int i = 0; i < str.length(); i++) {
+                if (Character.isLowerCase(str.charAt(i))) arr[0]++;
+                else if (Character.isUpperCase(str.charAt(i))) arr[1]++;
+                else if (Character.isDigit(str.charAt(i))) arr[2]++;
+                else arr[3]++;
+            }
+            sb.append(arr[0]).append(" ").append(arr[1]).append(" ").append(arr[2]).append(" ").append(arr[3]).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
