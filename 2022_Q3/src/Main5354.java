ef8f6d15a0352fc1664a634da193a21752b00a2b
hiMAC <hoh3@mail.csuchico.edu>
2022-10-12 21:52:01 +0900

    20221012

--- 2022_Q3/src/Main5354.java
+++ 2022_Q3/src/Main5354.java
@@ -4,18 +4,26 @@ public class Main5354 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
         for(int i = 0; i < N; i++) {
             int value = sc.nextInt();
-            for(int j = 0; j < value; j++) System.out.print("#");
-            System.out.println();
-            for(int j = 0; j < value - 2; j++) {
-                System.out.print("#");
-                for(int k = 0; k < value - 2; k++) System.out.print("J");
-                System.out.println("#");
+            if(value == 1) sb.append("#");
+            else {
+                for(int j = 0; j < value; j++) sb.append("#");
+                sb.append("\n");
+                for(int j = 0; j < value - 2; j++) {
+                    sb.append("#");
+                    for(int k = 0; k < value - 2; k++) sb.append("J");
+                    sb.append("#");
+                    sb.append("\n");
+                }
+                for(int j = 0; j < value; j++) sb.append("#");
             }
-            if(value != 1) for(int j = 0; j < value; j++) System.out.print("#");
-            System.out.println();
-            if(value == 3 || value == 2) System.out.println();
+            if(i != N-1) {
+                sb.append("\n");
+                sb.append("\n");
+            }
         }
+        System.out.println(sb.toString());
     }
 }
