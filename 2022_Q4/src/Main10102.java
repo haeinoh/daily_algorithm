c90adb0f0b0d70d28db1cbf213a5f67e2a0ab57b
hiMAC <hoh3@mail.csuchico.edu>
2022-10-25 07:47:46 +0900

    20221025

--- 2022_Q4/src/Main10102.java
+++ 2022_Q4/src/Main10102.java
@@ -5,7 +5,7 @@ public class Main10102 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         String str = sc.next();
-        
+
         str = str.replaceAll("A", "");
         if(str.length() < (n - str.length())) System.out.println("A");
         else if(str.length() >  (n - str.length())) System.out.println("B");
