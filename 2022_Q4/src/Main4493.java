9f65fb78fbe1262def066729cc8875b3f3e2c551
hiMAC <hoh3@mail.csuchico.edu>
2022-11-05 19:12:49 +0900

    20221105

---
+++ 2022_Q4/src/Main4493.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main4493 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int t = 0; t < T; t++) {
+            int n = sc.nextInt();
+            for(int j = 0; j < n; j++) {
+
+            }
+        }
+    }
+}
