b55efe9fb71ba1d286ab863f004ee42da55be9ed
hiMAC <hoh3@mail.csuchico.edu>
2022-11-05 19:15:59 +0900

    20221105

--- 2022_Q4/src/Main4493.java
+++ 2022_Q4/src/Main4493.java
@@ -6,9 +6,19 @@ public class Main4493 {
         int T = sc.nextInt();
         for(int t = 0; t < T; t++) {
             int n = sc.nextInt();
+            int a = 0;
+            int b = 0;
             for(int j = 0; j < n; j++) {
-
+                String aa = sc.next();
+                String bb = sc.next();
+                if(aa.equals(bb)) continue;
+                if((aa.equals("R") && bb.equals("P")) || (aa.equals("P") && bb.equals("S")) ||
+                        (aa.equals("S") && bb.equals("R"))) b++;
+                else a++;
             }
+            if(a < b) System.out.println("Player 2");
+            else if(a > b) System.out.println("Player 1");
+            else System.out.println("TIE");
         }
     }
 }
