cea5095c505d16368ad08da30cac102f787d73aa
hiMAC <hoh3@mail.csuchico.edu>
2022-12-16 07:10:43 +0900

    20221216

---
+++ 2022_Q4/src/Main20254.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main20254 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt()*56 + sc.nextInt()*24 +
+                sc.nextInt()*14 + sc.nextInt()*6);
+    }
+}
