3c1b4ab286fc358c68e94c4192f4d47b7e95a84c
hiMAC <hoh3@mail.csuchico.edu>
2023-01-07 23:05:21 +0900

    20220107

--- 2023_Q1/src/Main25191.java
+++ 2023_Q1/src/Main25191.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
-public class Main15734 {
+public class Main25191 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int c = sc.nextInt();
 
-
+        System.out.println(Math.min(n, (a/2 + b)));
     }
 }
