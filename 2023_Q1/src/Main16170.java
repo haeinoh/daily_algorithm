eacada457f23aa50358131ccd2e8315d8d61b7cb
hiMAC <hoh3@mail.csuchico.edu>
2023-01-16 23:47:35 +0900

    20230116

---
+++ 2023_Q1/src/Main16170.java
@@ -0,0 +1,10 @@
+import java.util.Calendar;
+
+public class Main16170 {
+    public static void main(String[] args) {
+        Calendar cal = Calendar.getInstance();
+        System.out.println(cal.get(cal.YEAR));
+        System.out.println(cal.get(cal.MONTH)+1);
+        System.out.println(cal.get(cal.DATE));
+    }
+}
