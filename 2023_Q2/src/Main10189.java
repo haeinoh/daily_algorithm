4ed50f78aa8b75fd62bca40320832e48ed76f45e
hiMAC <hoh3@mail.csuchico.edu>
2023-04-28 22:32:43 +0900

    20230426

---
+++ 2023_Q2/src/Main10189.java
@@ -0,0 +1,9 @@
+public class Main10189 {
+    public static void main(String[] args) {
+        System.out.println("#  # #### #### #  #");
+        System.out.println("#### #  # #  # # #");
+        System.out.println("#### #  # #  # # #");
+        System.out.println("#  # #### #### #  #");
+
+    }
+}
