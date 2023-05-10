a3d6e8df13dac489cc0138e5cff18e23ab70dc36
hiMAC <hoh3@mail.csuchico.edu>
2023-05-10 22:37:00 +0900

    20230510

---
+++ 2023_Q2/src/Main10101.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main10101 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if(a == b  && b == c  && c == 60) System.out.println("Equilateral");
+        else if(a + b + c == 180 && ( a == b || b == c || a == c)) System.out.println("Isosceles");
+        else if(a + b + c == 180 && (a != b && b != c)) System.out.println("Scalene");
+        else System.out.println("Error");
+    }
+}
