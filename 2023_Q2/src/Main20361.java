6b34ab158763aaa52575b8a4056d6a2f1bac21f5
hiMAC <hoh3@mail.csuchico.edu>
2023-05-15 20:56:37 +0900

    20230515

---
+++ 2023_Q2/src/Main20361.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main20361 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+        int k = sc.nextInt();
+
+        for(int i = 0; i < k; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if(a == x) x = b;
+            else if(b == x) x = a;
+        }
+        System.out.println(x);
+    }
+}
