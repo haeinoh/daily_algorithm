492590ea12acfd6c64bfb670ca091dfa1f30c270
hiMAC <hoh3@mail.csuchico.edu>
2023-06-03 20:12:59 +0900

    20230603

---
+++ 2023_Q2/src/Main27327.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Main27327 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt()*24);
+    }
+}
