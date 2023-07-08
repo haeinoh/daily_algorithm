1e9dd4e6365ee4f22116e9bff20e914faa00ab72
hiMAC <hoh3@mail.csuchico.edu>
2023-07-08 22:24:43 +0900

    20230708

---
+++ 2023_Q3/src/Main16466.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main16466 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        int min = 1;
+        for(int i = 0; i < n; i++) {
+            if(arr[i] == min) min++;
+        }
+        System.out.println(min);
+    }
+}
